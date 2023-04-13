7872ad756deade98a3d770c3e06134470a216560
davidecaputo <daca9621@gmail.com>
2023-04-13 17:06:05 +0200

    Add images

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
+  <div class="d-flex flex-column">
+    <span>Prova</span>
+    <span>Prova</span>
+  </div>
 </template>
 
 <style scoped>
