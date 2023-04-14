e24a3fa0fd6d1fb76dac60accd978e3453699fdf
davidecaputo <daca9621@gmail.com>
2023-04-14 10:05:57 +0200

    add element to main

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
 import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 </script>
 
 <template>
   <HeaderComponent />
+  <MainComponent />
 </template>
 
 <style scoped>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+        <div class="bg-dark">
+            <div class="container p-5">
+                <span class="text-white fs-2">-- Content goes here --</span>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
